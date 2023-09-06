1a3ef7899a2e11ba87810aaa63c62a671c93ac10
arha06001 <arha06001@E-PW0234JA>
2023-09-06 10:47:15 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Atte
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+         Console.WriteLine("Du har lärt dig följande tecken \" och \\.");
+
+            //gör så att programmet kommer skriva ut texten Hello world när det körs, den finns alltså med från början i varje program man skapar.För att testa ditt program så behöver du bara trycka på knappen med en grön play i verktygsfältet (eller genom menyn Debug -> Start Debugging eller genom att tryckapå F5).
+        }
+    }
+}
